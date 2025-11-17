4fc643132074e9c4ef4a936aa86f771ec9d78012
ayannar2014 <arodriguez021480@gmail.com>
2025-11-17 19:08:01 +0000

    lists_functions

--- week 13  csp/3 — Lists and List Functions.py
+++ week 13  csp/3 — Lists and List Functions.py
@@ -3,22 +3,72 @@
 
 # Topics Covered:
 # Creating lists, indexing, slicing, appending, popping, sorting, reversing.
+###################################################################################################
+# collections are covered to store multiple items in a single variable
+# lists are ordered collections of items
+# lists are mutable, meaning you can change their content
+# lists are created using square brackets []
+
+list_of_fruits = ["apple", "banana", "cherry", "date"]
+print (list_of_fruits) # ['apple', 'banana', 'cherry', 'date']
+print(type(list_of_fruits)) 
+#Acessing items in a list
+print(list_of_fruits[0])
+print(list_of_fruits[1]) 
+print (list_of_fruits[-1])
+print(list_of_fruits[1:3]) 
+#reverse the list
+list_of_fruits.reverse() 
+print(list_of_fruits[::-1]) 
+#appending items to a list
+list_of_fruits.append("elderberry")
+print(list_of_fruits)
+list_of_fruits.append("strawberry")
+print(list_of_fruits)
+list_of_fruits.append("plum")
+print(list_of_fruits)
+#to add multiple items in a list
+list_of_fruits.extend(["honeyfruit", "dragonfruit", "grape"])
+print(list_of_fruits)
+#revrse a list
+list_of_fruits.reverse()
+print(list_of_fruits)
+
+#popping item from a list
+popped_item = list_of_fruits.pop()
+print(popped_item)
+print(list_of_fruits)
+#inserting item at specific index
+list_of_fruits.insert(1,"blueberry")
+print(list_of_fruits) 
+#removing a specific item
+list_of_fruits.remove("banana") 
+print(list_of_fruits) #['grape', 'blueberry', 'dragonfruit', 'honeyfruit', 'plum', 'strawberry', 'elderberry', 'apple', 'cherry']
+list_of_fruits.insert(3,"banana")
+list_of_fruits.sort()
+print(list_of_fruits)
+
+list_of_items = list(range(1,101))
+print(list_of_items) 
+list_of_items = list(range(1,1001))
+print(list_of_items) 
+print(len(list_of_items))
 
 # Examples:
 
-my_list = ['apple', 'banana', 'cherry']
-print(my_list[0])         # apple
-print(my_list[1:])        # ['banana', 'cherry']
+# my_list = ['apple', 'banana', 'cherry']
+# print(my_list[0])         # apple
+# print(my_list[1:])        # ['banana', 'cherry']
 
-my_list.append('grape')
-print(my_list)
+# my_list.append('grape')
+# print(my_list)
 
-my_list.pop(1)
-print(my_list)
+# my_list.pop(1)
+# print(my_list)
 
-numbers = [3, 1, 4, 2]
-numbers.sort()
-print(numbers)
+# numbers = [3, 1, 4, 2]
+# numbers.sort()
+# print(numbers)
 
 
 # Practice Problems:
